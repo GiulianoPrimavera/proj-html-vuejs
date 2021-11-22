c313c828981260c048b0a5a3aefd215efdad36d0
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-22 13:56:34 +0100

    format comment

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,63 @@
 <template>
   <div id="app">
     <Header>
-    <!-- navbar -->
-    <!-- hamburgher menu --> 
-    </Header> 
-
+      <!-- navbar -->
+      <!-- hamburgher menu -->
+    </Header>
 
     <Main>
-
-    <!-- Sezione Hero
+      <!-- Sezione Hero
         titolo
         sottotitolo
         bottoni link -->
 
-    <!-- Sezione Brake
+      <!-- Sezione Brake
         titolo
         sottotitolo -->
 
-    <!-- Sezione Notizie 
+      <!-- Sezione Notizie 
         titolo
         sottotitolo
         cards notizie-->
 
-    <!-- Sezione Play Sample 
+      <!-- Sezione Play Sample 
         testo
         bottone play-->
 
-    <!-- Sezione Live Dates 
+      <!-- Sezione Live Dates 
         titolo
         sottotitolo
         ul delle date
         li accordion-->
-    
     </Main>
 
-
     <Footer>
-        <!-- navbar footer -->
-        <!-- copyright e social link -->
+      <!-- navbar footer -->
+      <!-- copyright e social link -->
     </Footer>
   </div>
 </template>
 
 <script>
-import Header from "./components/Header.vue"
-import Main from "./components/Main.vue"
-import Footer from "./components/Footer.vue"
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
     Main,
-    Footer
-  }
-}
+    Footer,
+  },
+};
 </script>
 
 <style lang="scss">
+@import "styles/header";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
 }
 </style>
