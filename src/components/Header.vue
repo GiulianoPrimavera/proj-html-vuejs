4ca3f2bf4d92e95860736097b8e54434b3b70b30
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-22 15:44:51 +0100

    adds hamburger menu layout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,21 +6,35 @@
       </div>
 
       <div class="hamburger_menu">
-        <i class="fas fa-bars"></i>
+        <div class="hamburger_menu_icon">
+          <i class="fas fa-bars"></i>
+          <i class="fas fa-times"></i>
+        </div>
+
+        <div class="hamburger_menu_expand">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Meet The Band</a></li>
+            <li><a href="#">Live Dates</a></li>
+            <li><a href="#">Latest News</a></li>
+            <li><a href="#">Albums</a></li>
+            <li><a href="#">Fans</a></li>
+          </ul>
+        </div>
       </div>
     </div>
 
     <div class="hero">
-        <div class="hero_text">
-            <h1>Untold Stories</h1>
+      <div class="hero_text">
+        <h1>Untold Stories</h1>
 
-            <p><em>There is an untold story behind every favorite song.</em></p>
+        <p><em>There is an untold story behind every favorite song.</em></p>
 
-            <div class="hero_buttons">
-                <a href="#">LATEST ALBUM</a>
-                <a href="#">LIVE DATES</a>
-            </div>
+        <div class="hero_buttons">
+          <a href="#">LATEST ALBUM</a>
+          <a href="#">LIVE DATES</a>
         </div>
+      </div>
     </div>
   </div>
 </template>
