1aeaa1dd24be121ca576aaf42dec2ca680b36ea0
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-22 17:24:39 +0100

    adds the first two text sections for the main

---
+++ src/components/Main.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main">
+    <div class="brake">
+      <div class="main_text_section">
+        <h2>
+          Music Is Life
+        </h2>
+        <p class="small_text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos ea at explicabo adipisci doloremque quae non quam ab, vitae nisi porro, nihil quisquam id vel provident suscipit architecto dolores perspiciatis!</p>
+      </div>
+    </div>
+
+    <div class="news">
+      <div class="main_text_section">
+        <h2>
+          Latest Band News
+        </h2>
+        <p class="small_text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos ea at explicabo adipisci doloremque quae non quam ab, vitae nisi porro, nihil quisquam id vel provident suscipit architecto dolores perspiciatis!</p>
+      </div>
+    </div>
+
+    <div class="play_sample"></div>
+
+    <div class="live_dates"></div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Main",
+}
+</script>
+
+<style>
+
+</style>
