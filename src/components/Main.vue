660a73ce36a5a3ee43bcb0804cfc888e025492e5
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-23 13:37:58 +0100

    creates main layout for the news section using grid

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,20 +1,95 @@
 <template>
   <div class="main">
     <div class="brake">
+      <!-- sezione di testo (si ripete 3 volte nel codice) -->
       <div class="main_text_section">
-        <h2>
-          Music Is Life
-        </h2>
-        <p class="small_text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos ea at explicabo adipisci doloremque quae non quam ab, vitae nisi porro, nihil quisquam id vel provident suscipit architecto dolores perspiciatis!</p>
+        <!-- linea rossa -->
+        <div class="brake_red_line"></div>
+
+        <h2>Music Is Life</h2>
+        <p class="small_text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+          ea at explicabo adipisci doloremque quae non quam ab, vitae nisi
+          porro, nihil quisquam id vel provident suscipit architecto dolores
+          perspiciatis!
+        </p>
       </div>
     </div>
 
     <div class="news">
+      <!-- sezione titolo delle news -->
       <div class="main_text_section">
-        <h2>
-          Latest Band News
-        </h2>
-        <p class="small_text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos ea at explicabo adipisci doloremque quae non quam ab, vitae nisi porro, nihil quisquam id vel provident suscipit architecto dolores perspiciatis!</p>
+        <h2>Latest Band News</h2>
+        <p class="small_text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+          ea at explicabo adipisci doloremque quae non quam ab, vitae nisi
+          porro, nihil quisquam id vel provident suscipit architecto dolores
+          perspiciatis!
+        </p>
+      </div>
+
+      <!-- contenitore delle news -->
+      <div class="news_container">
+        <div class="news_card card_1">
+          <div class="news_card_image_container">
+            <img src="" alt="" />
+          </div>
+
+          <div class="news_card_text_container">
+            <h3>Titolo</h3>
+            <p class="small_text">sottotitolo</p>
+          </div>
+        </div>
+        <div class="news_card card_2">
+          <div class="news_card_image_container">
+            <img src="" alt="" />
+          </div>
+
+          <div class="news_card_text_container">
+            <h3>Titolo</h3>
+            <p class="small_text">sottotitolo</p>
+          </div>
+        </div>
+        <div class="news_card card_3">
+          <div class="news_card_image_container">
+            <img src="" alt="" />
+          </div>
+
+          <div class="news_card_text_container">
+            <h3>Titolo</h3>
+            <p class="small_text">sottotitolo</p>
+          </div>
+        </div>
+        <div class="news_card card_4">
+          <div class="news_card_image_container">
+            <img src="" alt="" />
+          </div>
+
+          <div class="news_card_text_container">
+            <h3>Titolo</h3>
+            <p class="small_text">sottotitolo</p>
+          </div>
+        </div>
+        <div class="news_card card_5">
+          <div class="news_card_image_container">
+            <img src="" alt="" />
+          </div>
+
+          <div class="news_card_text_container">
+            <h3>Titolo</h3>
+            <p class="small_text">sottotitolo</p>
+          </div>
+        </div>
+        <div class="news_card card_6">
+          <div class="news_card_image_container">
+            <img src="" alt="" />
+          </div>
+
+          <div class="news_card_text_container">
+            <h3>Titolo</h3>
+            <p class="small_text">sottotitolo</p>
+          </div>
+        </div>
       </div>
     </div>
 
@@ -26,10 +101,9 @@
 
 <script>
 export default {
-name: "Main",
-}
+  name: "Main",
+};
 </script>
 
 <style>
-
 </style>
